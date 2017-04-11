879d5ee3a851a7cda4a25d42ef6d906cddce4d2c
nesty <rajitha.wijayaratne@gmail.com>
2017-04-11 10:17:33 +1000

    added logging

--- start-app.sh
+++ start-app.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if [ "$NEST_CONTAINER_TYPE" != "test" ]; then
-  /bin/bash /usr/local/tree/nest/utils/start
+  /bin/bash /usr/local/tree/nest/utils/start |& tee /tmp/start.log
+else
+  echo "running in test mode" > /tmp/start.log
 fi
   
