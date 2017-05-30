931b157fef3ac6d1c15099faff3284e3bfbac6e7
Your Name <you@example.com>
2017-05-30 12:22:34 +0000

    removed test from staratup

--- start-app.sh
+++ start-app.sh
@@ -1,9 +1,8 @@
-
 #!/bin/bash
 
-if [ "$NEST_CONTAINER_TYPE" != "test" ] && [ -f /var/app/app.nest ]; then
-  /bin/bash /usr/local/tree/nest/utils/start |& tee /tmp/start.log
+if [ -f /var/app/app.nest ]; then
+	/bin/bash /usr/local/tree/nest/utils/start |& tee /tmp/start.log
 else
-  echo "running in test mode" > /tmp/start.log
+	tail -f /dev/null
 fi
   
