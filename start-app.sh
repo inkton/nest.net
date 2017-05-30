dd13d733e802598d0ddc0673075073c753130082
nesty <rajitha.wijayaratne@gmail.com>
2017-05-30 21:35:22 +1000

    added check for app.nest file

--- start-app.sh
+++ start-app.sh
@@ -1,6 +1,7 @@
+
 #!/bin/bash
 
-if [ "$NEST_CONTAINER_TYPE" != "test" ]; then
+if [ "$NEST_CONTAINER_TYPE" != "test" ] && [ -f /var/app/app.nest ]; then
   /bin/bash /usr/local/tree/nest/utils/start |& tee /tmp/start.log
 else
   echo "running in test mode" > /tmp/start.log
